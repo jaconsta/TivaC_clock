ac5a29e9eb24bff379d97a41d9e98e1e6f36442e
Javier Constain <constainopia@gmail.com>
2014-10-22 13:53:24 -0500

    Added comments

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
-// Clock, main.c
+//! Clock, main.c
+/********************************************//**
+ * 
+ * Base logic to run the clock in seconds, minutes and hours
+ * 
+ * Minutes run in 60 minutes binary format.
+ * Hours run in 12 hour decade mode.
+ * 
+ * Designed for the Texas Instruments Tiva C launchpad
+ * ARM Cortex M4 tm4c123gh6pm microcontroller.
+ * 
+ * Seconds use the Port F 3 pin
+ * Minutes use the Port A 2 - 7 pins
+ * Hour unit use the Port E 0-3 pins
+ * Hour tens use the Port F 2 pin
+ * 
+ * Author: Javier Constain
+ * Creation Date: October 2014
+ * 
+ ***********************************************/
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -17,12 +36,11 @@ void flashSOS(void);
 void delay(unsigned long sec);
 void enable_interrupts(void);
 
-// Global variables
+/** Global variables */
 unsigned int portA_advance = 0x04;
 unsigned int portE_advance = 0x01;
 
 int main(void){
-	//unsigned int unit_seconds = 0;
 	portA_init();
 	portE_init();
 	portF_init();
@@ -41,51 +59,51 @@ int main(void){
 
 }
 
-//Initialize port A
+/** Initialize port A. */
 void portA_init(void) {
 	volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000001;		// 1. Enable Port Clock
+	SYSCTL_RCGC2_R |= 0x00000001;		// 1. Enable Port Clock
 	delay = SYSCTL_RCGC2_R;
-	GPIO_PORTA_CR_R |= 0xFC;				// Allow changes to P7A-2
-	GPIO_PORTA_AMSEL_R &= ~0xFC;			// 3. Disable analog function
-	GPIO_PORTA_PCTL_R &= ~0xFFFFFF00;// 4. GPIO clear bit PCTL
-	GPIO_PORTA_DIR_R |= 0xFC;				// 5.2 P7A-2 as output
-	GPIO_PORTA_AFSEL_R &= ~0xFC;			// 6 Disable alternate function
-	GPIO_PORTA_DEN_R |= 0xFC;				// 7. Enable digital pins P7A-2
+	GPIO_PORTA_CR_R |= 0xFC;		// Allow changes to P7A-2
+	GPIO_PORTA_AMSEL_R &= ~0xFC;		// 3. Disable analog function
+	GPIO_PORTA_PCTL_R &= ~0xFFFFFF00;	// 4. GPIO clear bit PCTL
+	GPIO_PORTA_DIR_R |= 0xFC;		// 5.2 P7A-2 as output
+	GPIO_PORTA_AFSEL_R &= ~0xFC;		// 6 Disable alternate function
+	GPIO_PORTA_DEN_R |= 0xFC;		// 7. Enable digital pins P7A-2
 }
-//Initialize port E
+/** Initialize port E. */
 void portE_init(void){
 	volatile unsigned long delay;
 	
-	SYSCTL_RCGC2_R |= 0x10; // 1. Enable port clock
+	SYSCTL_RCGC2_R |= 0x10; 		// 1. Enable port clock
 	delay = SYSCTL_RCGC2_R;
-	GPIO_PORTE_CR_R |= 0x0F;				// Allow changes to PF3-0
-	GPIO_PORTE_AMSEL_R &= 0x00;			// 3. Disable analog function
-	GPIO_PORTE_PCTL_R &= 0x00000000;// 4. GPIO clear bit PCTL
-	GPIO_PORTE_DIR_R |= 0x0F;				// 5.2 PE3-0 as output
-	GPIO_PORTE_AFSEL_R &= 0x00;			// 6 Disable alternate function
-	//GPIO_PORTE_PUR_R &= 0x0F;				// Enable pull-up resistors on PE3-0
-	GPIO_PORTE_DEN_R |= 0x0F;				// 7. Enable digital pins PE3-0
+	GPIO_PORTE_CR_R |= 0x0F;		// Allow changes to PF3-0
+	GPIO_PORTE_AMSEL_R &= 0x00;		// 3. Disable analog function
+	GPIO_PORTE_PCTL_R &= 0x00000000;	// 4. GPIO clear bit PCTL
+	GPIO_PORTE_DIR_R |= 0x0F;		// 5.2 PE3-0 as output
+	GPIO_PORTE_AFSEL_R &= 0x00;		// 6 Disable alternate function
+	GPIO_PORTE_DEN_R |= 0x0F;		// 7. Enable digital pins PE3-0
 }
-//Initialize port F
+/** Initialize port F. */
 void portF_init(void) {
 	volatile unsigned long delay;
 	// For this microcontroller is a 7 step process to set each port
 	SYSCTL_RCGC2_R |= 0x00000020;		// 1. Enable Port Clock
 	delay = SYSCTL_RCGC2_R;
-	GPIO_PORTF_LOCK_R = 0x4C4F434B;	// 2. Unlock port F PF0
-	GPIO_PORTF_CR_R |= 0x1F;				// Allow changes to PF4-0
-	GPIO_PORTF_AMSEL_R &= 0x00;			// 3. Disable analog function
-	GPIO_PORTF_PCTL_R &= 0x00000000;// 4. GPIO clear bit PCTL
-	GPIO_PORTF_DIR_R	&= ~0x11;			// 5.1 PF4, PF0 as inputs
-	GPIO_PORTF_DIR_R |= 0x0E;				// 5.2 PF3 as output
-	GPIO_PORTF_AFSEL_R &= 0x00;			// 6 Disable alternate function
-	GPIO_PORTF_PUR_R &= 0x11;				// Enable pull-up resistors on PF4, PF0
-	GPIO_PORTF_DEN_R |= 0x1F;				// 7. Enable digital pins PF4, PF0
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;		// 2. Unlock port F PF0
+	GPIO_PORTF_CR_R |= 0x1F;		// Allow changes to PF4-0
+	GPIO_PORTF_AMSEL_R &= 0x00;		// 3. Disable analog function
+	GPIO_PORTF_PCTL_R &= 0x00000000;	// 4. GPIO clear bit PCTL
+	GPIO_PORTF_DIR_R	&= ~0x11;	// 5.1 PF4, PF0 as inputs
+	GPIO_PORTF_DIR_R |= 0x0E;		// 5.2 PF3 as output
+	GPIO_PORTF_AFSEL_R &= 0x00;		// 6 Disable alternate function
+	GPIO_PORTF_PUR_R &= 0x11;		// Enable pull-up resistors on PF4, PF0
+	GPIO_PORTF_DEN_R |= 0x1F;		// 7. Enable digital pins PF4, PF0
 }
 
-// Calls a delay
-// Assumes a 80 MHz clock
+/** Calls a delay. */
+/** Assumes a 80 MHz clock. */
+/** TODO: Adjust the delay to last one exact second. */
 void delay(unsigned long time){
 	unsigned long i;
 	
@@ -100,16 +118,18 @@ void delay(unsigned long time){
 	return;
 }
 
+/** Controls the minutes advance. */
 void shiftMinutes(){
   if((GPIO_PORTA_DATA_R & 0xF0) == 0xF0){
 		GPIO_PORTA_DATA_R &= ~0xFC;
 		shiftHours();
 		GPIO_PORTA_DATA_R |= portA_advance;
 	} else {
-		GPIO_PORTA_DATA_R += portA_advance; //<<= portA_advance;
+		GPIO_PORTA_DATA_R += portA_advance; 
 	}
 	return;
 }
+/** Controls the hour advance. */
 void shiftHours(){
 	if(((GPIO_PORTE_DATA_R & 0x0F) == 0x02) && ((GPIO_PORTF_DATA_R & 0x04) == 0x04)){
 		GPIO_PORTE_DATA_R &= ~0x0F;
